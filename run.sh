034c4e89335843b13cf0eec68a8beb78ce87a986
yifanzhang-pro <zhangyif21@mails.tsinghua.edu.cn>
2024-10-24 07:06:33 +0200

    merge

--- run.sh
+++ run.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-torchrun --standalone --nproc_per_node=4 train_gpt2_wandb.py
-=======
 torchrun --standalone --nproc_per_node=4 train_gpt2_adamw.py
->>>>>>> ad9eff1453dc149139abe2e1befcc2646b09cfa6
