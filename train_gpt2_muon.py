2f03e57fbf477c8fecdf81a47f4f9cd6f1fe942a
yifanzhang-pro <zhangyif21@mails.tsinghua.edu.cn>
2024-10-24 07:59:10 +0200

    merge

--- train_gpt2_muon.py
+++ train_gpt2_muon.py
@@ -357,16 +357,6 @@ class Hyperparameters:
     input_bin: str = 'data/fineweb10B/fineweb_train_*.bin'  # input .bin to train on
     input_val_bin: str = 'data/fineweb10B/fineweb_val_*.bin'  # input .bin to eval validation loss on
     # optimization hyperparams
-<<<<<<< HEAD
-    batch_size : int = 8*64 # batch size, in sequences, across all devices
-    device_batch_size : int = 64 # batch size, in sequences, per device
-    sequence_length : int = 1024 # sequence length, in tokens
-    num_iterations : int = 100000 # number of iterations to run
-    learning_rate : float = 0.0036
-    warmup_iters : int = 0
-    warmdown_iters : int = 0 # number of iterations of linear warmup/warmdown for triangular or trapezoidal schedule
-    weight_decay : float = 0
-=======
     batch_size: int = 8*64  # batch size, in sequences, across all devices
     device_batch_size: int = 64  # batch size, in sequences, per device
     sequence_length: int = 1024  # sequence length, in tokens
@@ -375,7 +365,6 @@ class Hyperparameters:
     warmup_iters: int = 0
     warmdown_iters: int = 0  # number of iterations of linear warmup/warmdown for triangular or trapezoidal schedule
     weight_decay: float = 0
->>>>>>> refs/remotes/origin/master
     # evaluation and logging hyperparams
     val_loss_every: int = 125  # every how many steps to evaluate val loss? 0 for only at the end
     val_tokens: int = 10485760  # how many tokens of validation data? it's important to keep this fixed for consistent comparisons
@@ -423,7 +412,7 @@ if master_process:
 # Initialize wandb and track hyperparameters
 if master_process:
     # Get the current date in MM_DD_YY format
-    current_date = datetime.now().strftime("%m_%d_%y")
+    current_date = datetime.now().strftime("%m_%d_%y_%H_%M")
 
     wandb.init(
         project="modded-nanogpt",  # Set your wandb project name
